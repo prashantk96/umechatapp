5a41a9f70303bd4d537a54f57dbabeb548a18c78
prash <prashantkirpan>
2024-05-16 13:25:09 +0530

    Update .gitignore to exclude specific files

--- lib/api_key_controller.dart
+++
@@ -1,5 +0,0 @@
-class ApiKeyController {
-  static const String webKey = 'AIzaSyDVCcMN9pWR7m8RSqUQ_8x6tVO6d69j3-Q';
-  static const String androidKey = 'AIzaSyCmsnm0kxCZwiLqmMJKSRXxPUxHR1x0s-g';
-  static const String iosKey = 'AIzaSyDoGNq-edYTZ-WRnX0uqBpilWQqcl3XiS0';
-}
